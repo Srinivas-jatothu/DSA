47128f9dae30e8da4005fe230dcf6ffe5aa548cc
Srinivas-jatothu <cs21b023@iittp.ac.in>
2025-03-19 21:39:56 +0530

    leetcode-3191. Minimum Operations to Make Binary Array Elements Equal to One I

--- Arrays/Minimum Operations to make binary array elements equals to 1.cpp
+++ Arrays/Minimum Operations to make binary array elements equals to 1.cpp
@@ -5,15 +5,15 @@ class Solution {
     public:
         int minOperations(vector<int>& nums) {
             int n=nums.size();
-            vector<bool>flipped(n,false);
+            vector<bool>flipped(n,false);    
             int Current_flipped=0;
             int res=0;
     
             for(int i=0;i<n;i++){
-                if(i>=3 && flipped[i-3]==true){
+                if(i>=3 && flipped[i-3]==true){    //if the last 3 elements are flipped, we need to flip the current element back to its original state
                     Current_flipped--;
                 }
-                if(Current_flipped%2 == nums[i]){
+                if(Current_flipped%2 == nums[i]){   //if the current element is not equal to 1, we need to flip it
                     if(i+3>n){
                         return -1;
                     }
