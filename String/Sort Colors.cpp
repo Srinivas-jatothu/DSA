b04f6eda7434bdadde780959bc8a9fac8bcf327d
Srinivas-jatothu <cs21b023@iittp.ac.in>
2025-05-17 23:04:58 +0530

    leetcode-75. Sort Colors

---
+++ String/Sort Colors.cpp
@@ -0,0 +1,14 @@
+// leetcode problem : https://leetcode.com/problems/sort-colors/description/?envType=daily-question&envId=2025-05-17
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        
+    }
+};
+
+/*
+Algorithm Explanation:
+1.use library function sort to sort the array in ascending order.
+2. The time complexity of this algorithm is O(n log n), where n is the number of elements in the array, as we are using a sorting algorithm.
+*/
